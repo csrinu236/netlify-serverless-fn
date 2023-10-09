c40ad755a1d94408d60e8d0000eae772515eca26
Chenna.Sreenu <csrinu236@gmail.com>
2023-10-09 18:44:43 +0530

    modified headers

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,14 @@
 // netlify-functions/hello.js
 exports.handler = async (event, context) => {
+  // console.log('🚀 ~ file: hello.js:3 ~ exports.handler= ~ context:', context);
+  // console.log('🚀 ~ file: hello.js:3 ~ exports.handler= ~ event:', event);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello, World!' }),
+    headers: {
+      'access-control-allow-origin': '*.netlify.app',
+      'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, ssotoken',
+    },
   };
 };
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,21 @@
 // netlify-functions/hello.js
 exports.handler = async (event, context) => {
+  // console.log('🚀 ~ file: hello.js:3 ~ exports.handler= ~ context:', context);
+  // console.log('🚀 ~ file: hello.js:3 ~ exports.handler= ~ event:', event);
+  // const headers = {
+  //   'Access-Control-Allow-Origin': '*', // Set the appropriate origin or '*' for any origin
+  //   'Content-Type': 'application/json',
+  //   'X-Testing': 'hello testing',
+  //   'Access-Control-Allow-Credentials': true,
+  // };
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Index jss' }),
+    headers: {
+      'access-control-allow-origin': '*.netlify.app',
+      'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, ssotoken',
+    },
   };
 };
