9c178f91dc220a2ebafc5f1613bafbd116ea535f
Chenna.Sreenu <csrinu236@gmail.com>
2023-10-10 11:09:00 +0530

    modified headers

--- functions/hello.js
+++ functions/hello.js
@@ -6,7 +6,7 @@ exports.handler = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello, World!' }),
     headers: {
-      'Access-Control-Allow-Origin': '*.netlify.app, http://localhost:3000',
+      'Access-Control-Allow-Origin': 'https://react-subdomain.netlify.app',
       'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type',
       'Access-Control-Allow-Credentials': true,
