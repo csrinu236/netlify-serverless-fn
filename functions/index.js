69dac04fd503f2a75d1a1f8d711da9b6348c16db
Chenna.Sreenu <csrinu236@gmail.com>
2023-10-10 11:12:15 +0530

    modified headers

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,7 @@ exports.handler = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify({ message: 'Index jss' }),
     headers: {
-      'Access-Control-Allow-Origin': '*.netlify.app',
+      'Access-Control-Allow-Origin': 'https://*.netlify.app',
       'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type',
       'Access-Control-Allow-Credentials': true,
