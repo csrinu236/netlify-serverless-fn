d8556289aad42b83926ed16700c2402fe99f67a3
Chenna.Sreenu <csrinu236@gmail.com>
2023-10-10 14:23:51 +0530

    added conditional headers

--- functions/hello.js
+++ functions/hello.js
@@ -2,11 +2,17 @@
 exports.handler = async (event, context) => {
   // console.log('🚀 ~ file: hello.js:3 ~ exports.handler= ~ context:', context);
   // console.log('🚀 ~ file: hello.js:3 ~ exports.handler= ~ event:', event);
+
+  let ACAO = '*';
+  const requestOrigin = event.headers.origin;
+  if (requestOrigin && requestOrigin.endsWith('.netlify.app')) {
+    ACAO = requestOrigin;
+  }
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello, World!' }),
     headers: {
-      'Access-Control-Allow-Origin': 'https://react-subdomain.netlify.app',
+      'Access-Control-Allow-Origin': ACAO,
       'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type',
       'Access-Control-Allow-Credentials': true,
--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,25 @@
 // netlify-functions/hello.js
 exports.handler = async (event, context) => {
   // console.log('🚀 ~ file: hello.js:3 ~ exports.handler= ~ context:', context);
-  // console.log('🚀 ~ file: hello.js:3 ~ exports.handler= ~ event:', event);
+  console.log('🚀 ~ file: hello.js:3 ~ exports.handler= ~ event:', { event });
   // const headers = {
   //   'Access-Control-Allow-Origin': '*', // Set the appropriate origin or '*' for any origin
   //   'Content-Type': 'application/json',
   //   'X-Testing': 'hello testing',
   //   'Access-Control-Allow-Credentials': true,
+
   // };
 
+  let ACAO = '*';
+  const requestOrigin = event.headers.origin;
+  if (requestOrigin && requestOrigin.endsWith('.netlify.app')) {
+    ACAO = requestOrigin;
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Index jss' }),
+    body: JSON.stringify({ message: 'Index, World!' }),
     headers: {
-      'Access-Control-Allow-Origin': '*.netlify.app',
+      'Access-Control-Allow-Origin': ACAO,
       'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type',
       'Access-Control-Allow-Credentials': true,
